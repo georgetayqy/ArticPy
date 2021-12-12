08d8b5f014bd411c63b6b79d3aca1b40b2a9b16d
asdfghjkxd <73705042+asdfghjkxd@users.noreply.github.com>
2021-12-12 17:13:29 +0800

    Implemented NLP Model Trainer Functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # CUSTOM PAGE IMPORTS
 from pyfiles.multipage import MultiPage
-from pyfiles.pages import load_clean_visualise, document_term_matrix, toolkit_nlp
+from pyfiles.pages import load_clean_visualise, document_term_matrix, toolkit_nlp, model_trainer
 
 # INSTANTIATE THE APP
 app = MultiPage()
@@ -9,7 +9,7 @@ app = MultiPage()
 app.add_page('Load, Clean and Visualise Data', load_clean_visualise.app)
 app.add_page('DTM and Word Frequency Analysis', document_term_matrix.app)
 app.add_page('NLP Toolkit', toolkit_nlp.app)
-
+app.add_page('NLP Model Trainer', model_trainer.app)
 
 # RUN THE APP
 app.run()

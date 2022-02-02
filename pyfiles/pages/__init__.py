014ee3cf55fc567b75b5de83b83ecee7d951923b
George Tay <73705042+asdfghjkxd@users.noreply.github.com>
2022-02-03 00:26:00 +0800

    Massive Restructuring for Nginx

--- pyfiles/pages/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This module runs when the pyfiles apps are imported, downloading the corpora needed in this app"""
-
-import streamlit as st
-import nltk
-
-with st.spinner('Downloading WordNet Corpora...'):
-    nltk.download('wordnet')
-    nltk.download('vader_lexicon')
